d1a3faa9340bab2c7629d27ddc47fa1e07a3e148
Chris <chris.gastaldo@gmail.com>
2024-09-22 11:17:22 -0400

    completed functionality of create new entry with moddal that appears from create new entry button

--- src/buttons.js
+++ src/buttons.js
@@ -1,4 +1,4 @@
-
+import Todo from "./todo";
 
 export function deleteButtonFunctionality(){
     const deleteButtons = document.querySelectorAll('.deleteBtn');
@@ -15,6 +15,7 @@ export function newEntryFunctionality(){
     const newEntryButton = document.querySelector('.newEntry');
     const dialog = document.querySelector('dialog');
     const closeButton = document.querySelector('#closeButton');
+    const submitButton = document.querySelector('#submitButton');
 
     newEntryButton.addEventListener('click', ()=>{
         dialog.showModal();
@@ -24,6 +25,28 @@ export function newEntryFunctionality(){
         dialog.close();
     });
 
+    submitButton.addEventListener('click', function(){
+        createEntryFromForm();
+        dialog.close();
+    });
+
+}
+
+function createEntryFromForm(){
+    const formTitle = document.querySelector('#formTitle').value;
+    const formDate = document.querySelector('#formDate').value;
+    const formPriority = document.querySelector('#formPriority').value;
+    const formDescription = document.querySelector('#formDescription').value;
+
+    let titleEntry = formTitle;
+    let dateEntry = formDate;
+    let priorityEntry = formPriority;
+    let descriptionEntry = formDescription;
+
+    
+    const formEntry = new Todo(titleEntry, descriptionEntry,
+        dateEntry, priorityEntry);
+    formEntry.format();
 }
 
 function editButtonFunctionality(){
